f667c651d0e5ee6024833dbf4d90ad08a093e8d6
Swager0932 <swagerbot2000@gmail.com>
2024-06-18 17:24:32 -0400

    router setup

--- doula/app/page.tsx
+++ doula/app/page.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image'
+import {Button} from "@/components/ui/button"
 
 export default function Home() {
   return (
-<p className="text-3xl font-extrabold text-sky-600" >hi</p>
+    <div>
+      <p className="text-3xl font-extrabold text-sky-600" >hi</p>
+      <Button variant="destructive"> 
+        oh wussup
+      </Button>
+    </div>
   )
 }
---
+++ doula/app/test/test.tsx
@@ -0,0 +1,9 @@
+const TestPage = () => {
+    return ( 
+        <div>
+            This a test page fr
+        </div>
+     );
+}
+ 
+export default TestPage;
