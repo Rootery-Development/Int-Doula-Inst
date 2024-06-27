658e3e427525785837e856b7282de0f8185ef7f2
Swager0932 <swagerbot2000@gmail.com>
2024-06-27 19:25:49 -0400

    finally got the GetUnique error to stop

--- doula/app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ doula/app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -3,7 +3,7 @@ import { Chapter, Course, UserProgress } from "@prisma/client"
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
-// import { CourseProgress } from "@/components/course-progress";
+import { CourseProgress } from "@/components/course-progress";
 // edit
 import { CourseSidebarItem } from "./course-sidebar-item";
 
@@ -43,11 +43,11 @@ export const CourseSidebar = async ({
         </h1>
         {purchase && (
           <div className="mt-10">
-            {/* <CourseProgress
+            <CourseProgress
               variant="success"
               value={progressCount}
-            /> */}
-          </div> // edit
+            />
+          </div>
         )}
       </div>
       <div className="flex flex-col w-full">
