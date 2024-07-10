602a30b257f05dd9a66331ae012621cab6507a02
Swager0932 <swagerbot2000@gmail.com>
2024-07-09 21:42:02 -0400

    basic color coding

--- doula/app/(dashboard)/_components/sidebar-item.tsx
+++ doula/app/(dashboard)/_components/sidebar-item.tsx
@@ -25,21 +25,21 @@ export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
       onClick={onClick}
       type="button"
       className={cn(
-        "flex w-full items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+        "flex w-full items-center gap-x-2 text-white text-sm font-[500] pl-6 transition-all hover:text-slate-700 hover:bg-slate-300/20",
         isActive &&
-          "text-sky-700 g-sky200/20 hover:bg-sky-200/20 hover:text-sky-700"
+          "text-white g-sky200/20 hover:bg-sky-200/20 hover:text-sky-200"
       )}
     >
       <div className="flex items-center gap-x-2 py-4">
         <Icon
           size={22}
-          className={cn("text-slate-500", isActive && "text-sky-700")}
+          className={cn("text-sky-200", isActive && "text-sky-200")}
         />
         {label}
       </div>
       <div
         className={cn(
-          "ml-auto opacity-0 border-2 border-sky-700 h-full pt-14 transition-all",
+          "ml-auto opacity-0 border-8 border-sky-800 h-full pt-16 transition-all",
           isActive && "opacity-100"
         )}
       />
